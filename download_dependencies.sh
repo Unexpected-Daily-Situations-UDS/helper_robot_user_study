cad74e1e5ee9ac4250edee0862ea11903436ce83
Yoan Sallami <ysallami@laas.fr>
2019-11-13 13:28:30 +0100

    Initial commit

---
+++ download_dependencies.sh
@@ -0,0 +1,6 @@
+mkdir -p src/jspsych-6.1.0
+cd src/jspsych-6.1.0
+wget https://github.com/jspsych/jsPsych/releases/download/v6.1.0/jspsych-6.1.0.zip
+unzip jspsych-6.1.0.zip
+rm jspsych-6.1.0.zip
+cd ..
